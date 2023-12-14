80b5b2624faf2b850b57defbd725bfa85c23479b
flavien <day.flavien@gmail.com>
2023-12-14 03:59:19 +0100

    register user

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -4,8 +4,6 @@ import passportLocalMongoose from "passport-local-mongoose";
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
   role: { type: String, required: true, enum: ["Admin", "User"] },
 });
 UserSchema.plugin(passportLocalMongoose);
